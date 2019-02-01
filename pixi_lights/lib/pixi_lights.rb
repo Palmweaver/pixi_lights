1588bd8242f7b96687ecfc824792bec7d928605e
Palmweaver <thepalmweaver@gmail.com>
2019-02-01 08:45:01 -0500

    Initial commit

---
+++ pixi_lights/lib/pixi_lights.rb
@@ -0,0 +1,8 @@
+require "pixi_lights/version"
+
+module PixiLights
+  class Error < StandardError; end
+  class Engine < ::Rails::Engine;
+  	
+  end
+end
---
+++ pixi_lights/lib/pixi_lights/version.rb
@@ -0,0 +1,3 @@
+module PixiLights
+  VERSION = "0.1.0"
+end
